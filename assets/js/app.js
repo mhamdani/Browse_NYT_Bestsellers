3a07d1f0b10067fe22a7a2d77c22bc9380584661
Michelle Hamdani <michellehamdani@gmail.com>
2018-02-22 23:48:33 -0800

    NYT bestseller API

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+fetch('https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key=' + '2a6ff788cc214c088bc32b87e3894107', {
+    method: 'get',
+  })
+  .then(response => { return response.json(); })
+  .then(json => {
+    updateBestSellers(json);
+    console.log(json);
+  })
+  .catch(error => {
+    console.log('NYT API Error.' + error);
+  });
+
+function updateBestSellers(data){
+  data.results.forEach(function(book){
+    var bookInfo = book.book_details[0];
+    var listing =
+    '<div class="card">' +
+    '<h2><a href="' + book.amazon_product_url + '" target="_blank">' + bookInfo.title + '</a></h2>' +
+          '<h4>By ' + bookInfo.author + '</h4>' +
+          '<p>' + bookInfo.description + '</p>'+
+        '</div>';
+
+    $('#best-seller-titles').append(listing);
+  });
+}
